b983351ad5a758500227bd1d7d19d0353f7067d0
gabrielmendes <gabrielmendes151@gmail.com>
2020-06-30 22:09:25 -0300

    add save / list with balance of transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface Response {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +14,63 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance | null;
+
   constructor() {
     this.transactions = [];
+    this.balance = null;
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): Response {
+    const incomes = this.transactions.filter(item => item.type === 'income');
+    const outcomes = this.transactions.filter(item => item.type === 'outcome');
+
+    const totalIncome =
+      incomes.length > 0
+        ? incomes.map(item => item.value).reduce((accum, curr) => accum + curr)
+        : 0;
+
+    const totalOutcome =
+      outcomes.length > 0
+        ? outcomes.map(item => item.value).reduce((accum, curr) => accum + curr)
+        : 0;
+
+    const a = this.transactions.reduce((acc, curr) => {
+      return acc.value + curr.value;
+    });
+
+    const total = totalIncome - totalOutcome;
+    const balance = this.getBalance({
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    });
+
+    const response = {
+      transactions: this.transactions,
+      balance,
+    };
+    return response;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance({ income, outcome, total }: Balance): Balance {
+    this.balance = {
+      income,
+      outcome,
+      total,
+    };
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ id, title, value, type }: Transaction): Transaction {
+    const transactional = {
+      id,
+      title,
+      value,
+      type,
+    };
+    this.transactions.push(transactional);
+    return transactional;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,13 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transactional = new Transaction({ title, value, type });
+    const response = this.transactionsRepository.all();
+    if (response.balance.total - value < 0 && type === 'outcome') {
+      throw new Error('Invalid Balance.');
+    }
+    return this.transactionsRepository.create(transactional);
   }
 }
 
